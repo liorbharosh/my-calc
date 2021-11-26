823d3650da8e4eb090f42c0d00234c3f7df06232
liorbharosh <88676300+liorbharosh@users.noreply.github.com>
2021-11-26 23:04:14 +0200

    added absolute and power math operations
    added the abs operation and the pow operation, including some additional internal helper functions such as _floor and _decimalToFraction

--- solution/src/operations.ts
+++ solution/src/operations.ts
@@ -18,12 +18,21 @@ export function div(a: number, b: number): number {
     return a / b;
 }
 
-function _integersOnlyPow(num: number, integerExp: number): number {
+
+export function abs(num: number): number {
+    if (num < 0) return -num;
+    return num;
+}
+
+
+function _integerExponentOnlyPow(num: number, integerExp: number): number {
+    if (integerExp % 1 != 0) return global.NaN;
     if (num == 0 && integerExp == 0) return global.NaN;
     let result = 1;
-    for(let i = 0; i < integerExp; i++) {
+    for(let i = 0; i < abs(integerExp); i++) {
         result *= num;
     }
+    if (integerExp < 0) return 1 / result;
     return result;
 }
 
@@ -32,7 +41,33 @@ export function root(num: number, root: number): number {
     let estimate = 5;
     let numOfIterations = 1000;
     for (let i = 0; i < numOfIterations; i++) {
-        estimate = ((root-1)/root)*estimate + (num/root)*(1/(_integersOnlyPow(estimate, root-1)));
+        estimate = ((root-1)/root)*estimate + (num/root)*(1/(_integerExponentOnlyPow(estimate, root-1)));
     }
     return estimate;
 }
+
+
+function _floor(num: number): number {
+    let decimals = num % 1;
+    return num - decimals;
+}
+
+
+function _decimalToFraction(decimal: number) {
+    // eslint-disable-next-line no-var
+    for(var denominator = 1; (decimal * denominator) % 1 !== 0; denominator++);
+    return {numerator: decimal * denominator, denominator: denominator};
+}
+
+
+export function pow(num: number, exp: number): number {
+    if (num == 0 && exp == 0) return global.NaN;
+    if (exp % 1 == 0) return _integerExponentOnlyPow(num, exp);
+    let expNumerator = _decimalToFraction(exp).numerator;
+    let expDenominator = _decimalToFraction(exp).denominator;
+    return root(_integerExponentOnlyPow(num, expNumerator), expDenominator);
+}
+
+
+
+console.log(pow(3, 2.55));
