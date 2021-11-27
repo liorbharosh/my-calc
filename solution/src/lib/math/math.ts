e39c1ec713883cc7decc548cf9d526c10060edb9
liorbharosh <88676300+liorbharosh@users.noreply.github.com>
2021-11-27 12:38:37 +0200

    added math operations to the math lib

--- solution/src/lib/math/math.ts
+++ solution/src/lib/math/math.ts
@@ -1,1 +1,9 @@
 import * as operations from "./operations";
+
+export const add = operations.add;
+export const sub = operations.sub;
+export const mul = operations.mul;
+export const div = operations.div;
+export const ads = operations.abs;
+export const root = operations.root;
+export const pow = operations.pow;
