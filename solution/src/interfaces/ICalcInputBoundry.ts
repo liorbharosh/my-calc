5a54a6304ab8cb4aba07cbb64b810538a584f49a
liorbharosh <88676300+liorbharosh@users.noreply.github.com>
2021-11-29 22:36:57 +0200

    removed redundant method from calc input boundry

--- solution/src/interfaces/ICalcInputBoundry.ts
+++ solution/src/interfaces/ICalcInputBoundry.ts
@@ -2,6 +2,5 @@ import CalcRequestModel from "../classes/CalcRequestModel";
 
 export default interface ICalcInputBoundry {
     requestObject: CalcRequestModel;
-    sendRequst(): void;
 // eslint-disable-next-line semi
 }
