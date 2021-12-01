fd49549a9559d396e972f1b518aaa426c6f3cc4e
liorbharosh <88676300+liorbharosh@users.noreply.github.com>
2021-12-01 19:49:11 +0200

    added an operation class

---
+++ solution/src/classes/operation.ts
@@ -0,0 +1,11 @@
+class Operation {
+    readonly name;
+    readonly arg1;
+    readonly arg2;
+
+    constructor(name: string, arg1: number, arg2: number) {
+        this.name = name;
+        this.arg1 = arg1;
+        this.arg2 = arg2;
+    }
+}
